50c7a9d02234d05b1e5660be64afa9a836083d30
boyiin <boyin.jatto@gmail.com>
2023-09-20 16:53:48 +0100

    terraform project

---
+++ WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace Classic_DevOps_Deployment_Project_Test
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
